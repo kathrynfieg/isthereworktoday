35177ae242e3f5ae3fcedfdcc3fe85539b0c2f5f
Kathryn Fieg <kathrynfieg@gmail.com>
2024-09-21 23:02:28 +0930

    feat: initial implementation of API methods

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,82 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useQuery } from '@tanstack/vue-query';
+import { useGeolocation } from '@vueuse/core';
+import { NagerDateAUCounty } from '@/models';
+import {
+  getReversedLocation,
+  getAuPublicHolidays,
+  getIsTodayAuHoliday,
+} from '@/api';
 
-<template>IS IT A HOLIDAY</template>
+const { coords, error, isSupported } = useGeolocation();
+
+const isValidCoords = computed(() => {
+  const latitude = coords.value.latitude;
+  const longitude = coords.value.longitude;
+  return (
+    isSupported &&
+    typeof latitude === 'number' &&
+    typeof longitude === 'number' &&
+    isFinite(latitude) &&
+    isFinite(longitude)
+  );
+});
+
+const { data: userLocation, isLoading: isLoadingUserLocation } = useQuery({
+  queryKey: ['userLocation', coords, isValidCoords],
+  queryFn: () =>
+    getReversedLocation(coords.value.latitude, coords.value.longitude),
+  enabled: isValidCoords,
+});
+
+const { data: allPublicHolidays, isLoading: isLoadingPublicHolidays } =
+  useQuery({
+    queryKey: ['allPublicHolidays'],
+    queryFn: () => {
+      const currentYear = new Date().getFullYear();
+      return getAuPublicHolidays(currentYear);
+    },
+  });
+
+const { data: isTodayAuHoliday, isLoading: isLoadingTodayHoliday } = useQuery({
+  queryKey: ['isTodayAuHoliday'],
+  queryFn: () => getIsTodayAuHoliday(NagerDateAUCounty.SouthAustralia),
+});
+</script>
+
+<template>
+  <div>isLoadingUserLocation: {{ isLoadingUserLocation }}</div>
+  <div>isLoadingPublicHolidays: {{ isLoadingPublicHolidays }}</div>
+  <div>isLoadingTodayHoliday: {{ isLoadingTodayHoliday }}</div>
+  <br />
+
+  <div>error:{{ error?.message ? error.message : 'NO error' }}</div>
+
+  <br />
+
+  <div>
+    <strong>User Country: </strong>{{ userLocation?.address.country }} ({{
+      userLocation?.address.country_code
+    }})
+  </div>
+
+  <br />
+
+  <div><strong>User State: </strong>{{ userLocation?.address.state }}</div>
+
+  <br />
+
+  <div>
+    <strong>Is Today a holiday: </strong>{{ Boolean(isTodayAuHoliday) }}
+  </div>
+
+  <br />
+
+  <div>
+    <strong>All Public Holidays: </strong
+    >{{ allPublicHolidays?.map((holiday) => holiday.localName) }}
+  </div>
+</template>
 
 <style scoped></style>
