403c1d39488cfc38db01f222c0a797c53f9cc058
Kathryn Fieg <kathrynfieg@gmail.com>
2024-09-22 14:43:26 +0930

    refactor: cleanup

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,33 +1,32 @@
 import axios from 'axios';
-import {
-  LocationIQLocation,
-  NagerDateAUCounty,
-  NagerDateHoliday,
-} from '@/models';
+import { Location, Holiday, County, State, Territory } from '@/models';
+
+const locationIqApi = import.meta.env.VITE_LOCATIONIQ_API;
+const nagerDateApi = import.meta.env.VITE_NAGER_DATE_API;
 
 export const getReversedLocation = async (lat: number, lon: number) => {
   const apiKey = import.meta.env.VITE_LOCATIONIQ_API_KEY;
 
-  const endpoint = `${
-    import.meta.env.VITE_LOCATIONIQ_API
-  }?key=${apiKey}&lat=${lat}&lon=${lon}&format=json`;
+  const endpoint = `${locationIqApi}?key=${apiKey}&lat=${lat}&lon=${lon}&format=json`;
 
   try {
-    const response = await axios.get<LocationIQLocation>(endpoint);
+    const response = await axios.get<Location>(endpoint);
+    const address = response.data.address;
 
-    return response.data;
+    return {
+      ...address,
+      stateCode: getNagerDateCounty(address.territory ?? address.state),
+    };
   } catch (e) {
     throw e;
   }
 };
 
 export const getAuPublicHolidays = async (year: number) => {
-  const endpoint = `${
-    import.meta.env.VITE_NAGER_DATE_API
-  }/PublicHolidays/${year}/au`;
+  const endpoint = `${nagerDateApi}/PublicHolidays/${year}/au`;
 
   try {
-    const response = await axios.get<NagerDateHoliday[]>(endpoint);
+    const response = await axios.get<Holiday[]>(endpoint);
 
     return response.data;
   } catch (e) {
@@ -35,10 +34,10 @@ export const getAuPublicHolidays = async (year: number) => {
   }
 };
 
-export const getIsTodayAuHoliday = async (state: NagerDateAUCounty) => {
-  const endpoint = `${
-    import.meta.env.VITE_NAGER_DATE_API
-  }/IsTodayPublicHoliday/au?countyCode=${state}&offset=0`;
+export const getIsTodayAuHoliday = async (state?: County) => {
+  if (!state) return null;
+
+  const endpoint = `${nagerDateApi}/IsTodayPublicHoliday/au?countyCode=${state}&offset=0`;
 
   /**
    * response
@@ -47,9 +46,34 @@ export const getIsTodayAuHoliday = async (state: NagerDateAUCounty) => {
    */
 
   try {
-    const response = await axios.get<NagerDateHoliday>(endpoint);
+    const response = await axios.get<Holiday>(endpoint);
     return response.data;
   } catch (e) {
     throw e;
   }
 };
+
+export function getNagerDateCounty(
+  stateOrTerritory?: string
+): County | undefined {
+  switch (stateOrTerritory) {
+    case State.SouthAustralia:
+      return County.SouthAustralia;
+    case State.NorthernTerritory:
+      return County.NorthernTerritory;
+    case State.Queensland:
+      return County.Queensland;
+    case State.Tasmania:
+      return County.Tasmania;
+    case State.NewSouthWales:
+      return County.NewSouthWales;
+    case State.Victoria:
+      return County.Victoria;
+    case State.WesternAustralia:
+      return County.WesternAustralia;
+    case Territory.AustralianCapitalTerritory:
+      return County.AustralianCapitalTerritory;
+    default:
+      return undefined;
+  }
+}
