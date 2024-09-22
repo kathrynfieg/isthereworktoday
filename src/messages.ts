4b608aa3ff5b8a08a8c2653d5880d723a5c37802
Kathryn Fieg <kathrynfieg@gmail.com>
2024-09-22 16:11:52 +0930

    feat: add random responses

---
+++ src/messages.ts
@@ -0,0 +1,79 @@
+export const workdayMessages = [
+  "Yep, it's a workday. Time to pretend you're productive. 💼",
+  'Unfortunately, yes. The grind never stops. 😔',
+  'No holiday today, but coffee is your friend. ☕',
+  'It’s a regular day... unless you invent a holiday! 🎨',
+  'Not today, friend. Back to the grindstone. 🚧',
+  'Get that to-do list ready. It’s time to adult. 📝',
+  'Why aren’t you on a tropical beach somewhere? 🏖️',
+  "Rise and shine! It's work o'clock. ⏰",
+  'Your dreams of staying in bed have been canceled. 🛏️',
+  "Better pack lunch. It's a regular ol' workday. 🥪",
+  'Sorry, no holiday today. Guess you’re stuck with us. 🙃',
+  'Well, someone’s gotta pay the bills... 💸',
+  'Who needs a holiday when you have endless emails? 📧',
+  'Don’t worry, the weekend is coming. Eventually... ⏳',
+  'Time to hustle. Maybe tomorrow will be better. 🤞',
+  "It's workday central here. You knew it deep down. 🏢",
+  'Today’s forecast: 100% chance of working. ☁️',
+  'Another day, another dollar. Keep it up! 💪',
+  'The universe says... go to work. 🌌',
+  'You made it this far, a little more won’t hurt. 🏃‍♂️',
+];
+
+export const holidayMessages = [
+  'Woohoo! It’s a holiday! Time to relax. 🏖️',
+  'Yes! No work today. 🎉',
+  "Put the laptop away. It's time to chill. 🛋️",
+  'Holiday mode: activated. 🍹',
+  'Enjoy your day off! You earned it. 🌟',
+  'You’ve got the green light to do absolutely nothing. 🚦',
+  'Why work when you can holiday? 😎',
+  'The universe has gifted you a free day! 🎁',
+  'Time to do all the things... or none of the things. 🙌',
+  'It’s time to not set an alarm. Sleep in! 💤',
+  'Holiday today! Go have fun, or just do nothing. Your choice. 🎈',
+  'Relax. Work is overrated anyway. ✌️',
+  'Put on your comfy clothes. It’s a day for you. 🧦',
+  'Forget about work! The only thing on your to-do list is fun. 🎠',
+];
+
+export const loadingMessages = [
+  'Grabbing a calendar... will today be your lucky day?',
+  "Hold on a sec, we're checking if the world decided to give you a break today... 🕵️‍♂️",
+  'Just a moment! Let’s see if you can kick back today... ⏳',
+  'Hang tight! We’re digging into the holiday database... 🕵️‍♀️',
+  "Fetching the holiday intel! Don't go anywhere! 🚀",
+  'One moment please! Finding out if you get a lazy day... 🔍',
+  'Loading... contemplating your life choices while waiting... 🤔',
+  'Give us a second to check if the universe has blessed you with a day off! 🌌',
+  'Loading the good vibes... just a few more moments! 🌈',
+  'Loading your destiny... will it be a workday or holiday? 🎲',
+  'Hang tight! The holiday fairy is on the job... 🧚‍♀️',
+];
+
+export const errorMessages = [
+  'Oops! Looks like we hit a snag. Maybe the internet took a holiday? Please ensure your location is on and try again later.',
+  'Yikes! Something went wrong. Even computers need a day off sometimes! Please ensure your location is on and try again later.',
+  'Uh-oh! We couldn’t fetch the info. Let’s blame the weather! Please ensure your location is on and try again later.',
+  'Error 404: Holiday not found! Maybe it’s just being shy? Please ensure your location is on and try again later.',
+  'Looks like the holiday gods are not smiling today. Try again later! Please ensure your location is on and try again later.',
+  'Whoops! It seems like we took a wrong turn on the internet. Please ensure your location is on and try again later.',
+  'Uh-oh! Our holiday detector is malfunctioning. Don’t worry, it happens! Please ensure your location is on and try again later.',
+  'We tried to find the holiday, but it must be on vacation itself! Please ensure your location is on and try again later.',
+  'Looks like our holiday map is lost! Let’s find it together. Please ensure your location is on and try again later.',
+  'Something’s not quite right. Even the holidays need a break sometimes! Please ensure your location is on and try again later.',
+];
+
+export const notInAustraliaMessages = [
+  "Looks like you're not down under! This app is for Aussie folks only.",
+  "Oi! You're outside of Australia. Come back when you're in the land of kangaroos!",
+  'We love your enthusiasm, but this app is strictly for Australians.',
+  "Sorry mate, but you’re not in Australia. Try again when you're in the right time zone!",
+  'You’re on a different continent! This app only serves the wonderful land of Oz.',
+  'Hey there, global traveler! Unfortunately, this app is made just for those in Australia.',
+  'Wishing you safe travels, but this app is all about Aussie holidays!',
+];
+
+export const locationPermissionMessage =
+  'Please allow access to your location, so we can find out if today is a holiday! Your chill time depends on it! 🎉';
